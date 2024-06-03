e294a7eb9c35c2e37c9d332bee68b52ff2d55505
FJOnori <s1960329@ed.ac.uk>
2024-06-03 10:42:43 +0100

    Functions to dicts
    Improved efficiency by storing variables in dictionaries

--- plotHist.py
+++ plotHist.py
@@ -5,34 +5,51 @@ import csv
 class PlotCreator():
 
     def __init__(self) -> None:
-        self.CanvasSize  = 800
-        self.NoOfBins    = 50
-        self.legend_Y    = 0.75
-        self.legend_X    = 0.70
-        self.color_kpi   = ROOT.kBlue
-        self.color_pipi  = ROOT.kRed
-        self.color_kpisw = ROOT.kGreen
-        self.color_ratio = ROOT.kBlack
+        ROOT.gROOT.SetBatch(True)
 
+        self.CANVAS_SIZE_X   = 800
+        self.CANVAS_SIZE_Y   = 800
+        self.N_BINS          = 50
+        self.LEGEND_X        = 0.70
+        self.LEGEND_Y        = 0.75
+        self.LEGEND_SIZE_X   = 0.1
+        self.LEGEND_SIZE_Y   = 0.1
+        self.HIST_LINE_WIDTH = 2
+        self.FONT            = 43
+        self.AXIS_TITLE_SIZE = 20
+        self.LABEL_SIZE      = 15
+        self.MARKER_STYLE    = 20
+
+        self.COLOR           = {"kpi"  : ROOT.kBlue,
+                                "pipi" : ROOT.kRed,
+                                "kpisw": ROOT.kOrange,
+                                "ratio": ROOT.kBlack}
+        
         self.loadTrees()
         self.getVariableNames()
 
     def loadTrees(self):
+        # Uses sweights to remove background from sample data
         sweight = "(abs(B_M01-895.55)<100)*NB0_Kpigamma_sw"
+        self.Trees = {}
+
         # imports data from the .root files that contain Monte Carlo data
         # imports data from the Kaon decays and adds them to a TTree object
         self.Tree_kpi  = ROOT.TChain("DecayTree")
         self.Tree_kpi.Add("../HistogramsLHCbData/kpiG_MC_Bd2KstGamma_HighPt_prefilter_2018_noPIDsel-magdown.root")
         self.Tree_kpi.Add("../HistogramsLHCbData/kpiG_MC_Bd2KstGamma_HighPt_prefilter_2018_noPIDsel-magup.root")
-        
+        self.Trees["kpi"] = self.Tree_kpi
+
         # imports data from the Pion decays and adds them to a TTree object
         self.Tree_pipi  = ROOT.TChain("DecayTree")
         self.Tree_pipi.Add("../HistogramsLHCbData/pipiG_MC_Bd2RhoGamma_HighPt_prefilter_2018_noPIDsel-magdown.root")
         self.Tree_pipi.Add("../HistogramsLHCbData/pipiG_MC_Bd2RhoGamma_HighPt_prefilter_2018_noPIDsel-magup.root")
-        
+        self.Trees["pipi"] = self.Tree_pipi
+
         # imports data from the Sample decays and adds them to a TTree object
         self.Tree_kpisw = ROOT.TChain("DecayTree")
         self.Tree_kpisw.Add("../HistogramsLHCbData/Sample_Kpigamma_2018_selectedTree_with_sWeights_Analysis_2hg_Unbinned-Mask1.root")
+        self.Trees["kpisw"] = self.Tree_kpisw
 
         #Defines the lengths of the TTree objects
         self.kpisw_size     = self.Tree_kpisw.GetEntries()
@@ -73,89 +90,63 @@ class PlotCreator():
         self.lowerBound = float('%.1g' % HistBounds[0])
         self.upperBound = float('%.1g' % HistBounds[-1])
 
-    def createKpiswHist(self, variable):
+    def createHist(self, decay, variable):
         self.findVariableBound(variable)
-        h_kpisw = ROOT.TH1F(f"h_kpisw", f"{variable}",  self.NoOfBins, self.lowerBound, self.upperBound)
-        h_kpisw.SetLineColor(self.color_kpisw)
-        h_kpisw.SetLineWidth(2)
-        self.Tree_kpisw.Draw(f"{variable}>>h_kpisw")
-
-        self.h_kpisw = h_kpisw
-
-    def createKpiHist(self, variable):
-        self.findVariableBound(variable)
-        h_kpi = ROOT.TH1F(f"h_kpi", f"{variable}",  self.NoOfBins, self.lowerBound, self.upperBound)
-        h_kpi.SetLineColor(self.color_kpi)
-        h_kpi.SetLineWidth(2)
-        self.Tree_kpi.Draw(f"{variable}>>h_kpi")
-
-        self.h_kpi = h_kpi
-
-    def createPipiHist(self, variable):
-        self.findVariableBound(variable)
-        h_pipi = ROOT.TH1F(f"h_pipi", f"{variable}", self.NoOfBins, self.lowerBound, self.upperBound)
-        h_pipi.SetLineColor(self.color_pipi)
-        h_pipi.SetLineWidth(2)
-        self.Tree_pipi.Draw(f"{variable}>>h_pipi")
-        
-        self.h_pipi = h_pipi
+        hist = ROOT.TH1F(f"h_{variable}_{decay}", f"{variable}_{decay}",  self.N_BINS, self.lowerBound, self.upperBound)
+        hist.SetLineColor(self.COLOR[decay])
+        hist.SetLineWidth(self.HIST_LINE_WIDTH)
+        self.Trees[decay].Draw(f"{variable}>>h_{variable}_{decay}")
+        self.Histograms[decay] = hist
 
     def createRatioPlot(self):
-        h_ratio = self.h_pipi.Clone("h_ratio")
-        h_ratio.Divide(self.h_kpi)
+        #Defines and normalises Ratio plot
+        h_ratio = self.Histograms["pipi"].Clone("h_ratio")
+        h_ratio.Divide(self.Histograms["kpi"])
         h_ratio.Scale(self.Tree_kpi_size/self.Tree_pipi_size)
 
+        # Style for Ratio plot
         h_ratio.SetTitle("")
-        h_ratio.SetLineColor(self.color_ratio)
-        h_ratio.SetMarkerStyle(20)
+        h_ratio.SetLineColor(self.COLOR["ratio"])
+        h_ratio.SetMarkerStyle(self.MARKER_STYLE)
 
+        # Style for y axis of Ratio plot
         h_ratioy = h_ratio.GetYaxis()
         h_ratioy.SetTitle("Ratio")
-        h_ratioy.SetTitleSize(20)
-        h_ratioy.SetTitleFont(43)
-        h_ratioy.SetLabelFont(43)
-        h_ratioy.SetLabelSize(15)
+        h_ratioy.SetTitleSize(self.AXIS_TITLE_SIZE)
+        h_ratioy.SetTitleFont(self.FONT)
+        h_ratioy.SetLabelFont(self.FONT)
+        h_ratioy.SetLabelSize(self.LABEL_SIZE)
 
+        # Style for x axis of Ratio plot
         h_ratiox = h_ratio.GetXaxis()
-        h_ratiox.SetLabelFont(43)
-        h_ratiox.SetLabelSize(15)
-
+        h_ratiox.SetLabelFont(self.FONT)
+        h_ratiox.SetLabelSize(self.LABEL_SIZE)
         h_ratio.Draw("SAME")
-
-        self.h_ratio = h_ratio
+        
+        self.Histograms["ratio"] = h_ratio
     
     def createLegend(self):
-        legend = ROOT.TLegend(self.legend_X, self.legend_Y, self.legend_X + 0.15, self.legend_Y + 0.10)
-        legend.AddEntry(self.h_pipi,"B^{0}#rightarrow #rho^{0}#gamma")
-        legend.AddEntry(self.h_kpi, "B^{0}#rightarrow K^{*0}#gamma")
+        legend = ROOT.TLegend(self.LEGEND_X, self.LEGEND_Y, self.LEGEND_X + self.LEGEND_SIZE_X, self.LEGEND_Y + self.LEGEND_SIZE_Y)
+        legend.AddEntry(self.Histograms["pipi" ], "B^{0}#rightarrow #rho^{0}#gamma - MC")
+        legend.AddEntry(self.Histograms["kpi"  ], "B^{0}#rightarrow K^{*0}#gamma - MC")
+        legend.AddEntry(self.Histograms["kpisw"], "B^{0}#rightarrow K^{*0}#gamma - Sample")
         legend.SetBorderSize(0)
         legend.SetTextSize(0.032)
     
         self.legend = legend
 
     def createSingleImage(self, variable, decay = "kpi"):
-        self.canvas = ROOT.TCanvas("c", "canvas", self.CanvasSize, self.CanvasSize)
-        
-        if decay == "kpi" : 
-            self.createKpiHist(variable)
-            self.h_kpi.DrawNormalized("HISTO", norm=1)
-        elif decay == "pipi" : 
-            self.createPipiHist(variable)
-            self.h_pipi.DrawNormalized("HISTO", norm=1)
-        elif decay == "kpisw":
-            self.createKpiswHist(variable)
-            self.h_pipi.DrawNormalized("HISTO", norm=1)
-        else:
-            raise Exception(" \'decay\' must be ethier \'kpi(sw)\' or \'pipi\'.")
-
+        self.canvas = ROOT.TCanvas("c", "canvas", self.CANVAS_SIZE_X, self.CANVAS_SIZE_Y)
+        self.createHist(decay, variable)
+        self.Histograms[decay].DrawNormalized("HISTO", norm=1)
         #Draws and saves the full image
         self.canvas.SetGridx()
         self.canvas.Draw()
-        self.canvas.Print(f"imgs/plot_{variable}_{decay}_single.pdf")
+        self.canvas.Print(f"../HistogramsLHCbImgs/plot_{variable}_{decay}_single.pdf")
         self.canvas.Close()
 
     def createDoubleCanvas(self):
-        canvas = ROOT.TCanvas("c", "canvas", self.CanvasSize, self.CanvasSize)
+        canvas = ROOT.TCanvas("c", "canvas", self.CANVAS_SIZE_X, self.CANVAS_SIZE_Y)
 
         padHist = ROOT.TPad("padHist", "padHist", 0, 0.3, 1, 1.0)
         padHist.SetBottomMargin(0.05)
@@ -180,24 +171,28 @@ class PlotCreator():
 
         #Defines and Draws both histograms
         self.padHist.cd(0)
-        self.createKpiHist(variable)
-        self.createPipiHist(variable)
+        self.createHist("kpi"  , variable)
+        self.createHist("pipi" , variable)
+        self.createHist("kpisw", variable)
+
+        self.Histograms["kpi"].SetTitle(variable)
 
         #Sets the title and font size
-        h_kpiy = self.h_kpi.GetYaxis()
+        h_kpiy = self.Histograms["kpi"].GetYaxis()
         h_kpiy.SetTitle(f"{variable} distribution")
-        h_kpiy.SetTitleSize(20)
-        h_kpiy.SetTitleFont(43)
-        h_kpiy.SetLabelFont(43)
-        h_kpiy.SetLabelSize(15)
+        h_kpiy.SetTitleSize(self.AXIS_TITLE_SIZE)
+        h_kpiy.SetTitleFont(self.FONT)
+        h_kpiy.SetLabelFont(self.FONT)
+        h_kpiy.SetLabelSize(self.LABEL_SIZE)
 
-        h_kpix = self.h_kpi.GetXaxis()
-        h_kpix.SetLabelFont(43)
+        h_kpix = self.Histograms["kpi"].GetXaxis()
+        h_kpix.SetLabelFont(self.FONT)
         h_kpix.SetLabelSize(0.0)
 
         #Draws histograms
-        self.h_kpi.DrawNormalized("HISTO", norm=1)
-        self.h_pipi.DrawNormalized("HISTO SAME", norm=1)
+        self.Histograms["kpi"  ].DrawNormalized("HISTO", norm=1)
+        self.Histograms["kpisw"].DrawNormalized("HISTO SAME", norm=1)
+        self.Histograms["pipi" ].DrawNormalized("HISTO SAME", norm=1)
 
         #Defines and Draws the legend
         self.createLegend()
@@ -206,23 +201,22 @@ class PlotCreator():
         #Defines and draws the ratio plot
         self.padRatio.cd(0)
         self.createRatioPlot()
-        self.h_ratio.Draw("SAME")
+        self.Histograms["ratio"].Draw("SAME")
 
         #Draws and saves the full image
         self.canvas.Draw()
-        self.canvas.Print(f"imgs/plot_{variable}.pdf")
+        self.canvas.Print(f"../HistogramsLHCbImgs/plot_{variable}.pdf")
         self.canvas.Close()
 
     def createAllDoubleImages(self):
-        ROOT.gROOT.SetBatch(True)
         for variable in self.CommonVariables:
             self.createDoubleImage(variable)
 
     def computeDifference(self, variable):
         #Returns the P value
-        self.createPipiHist(variable)
-        self.createKpiHist(variable)
-        return self.h_kpi.Chi2Test(self.h_pipi)
+        self.createHist("pipi",variable)
+        self.createHist("kpi", variable)
+        return self.Histograms["kpi"].Chi2Test(self.Histograms["pipi"])
     
     def computeAllDifferences(self):
         diffDict = {}
@@ -230,7 +224,7 @@ class PlotCreator():
             diff = self.computeDifference(var)
             diffDict[var] = diff
         
-        file = open("data/Chi2Values.csv", "wb")
+        file = open("../HistogramsLHCbData/Chi2Values.csv", "wb")
         write = csv.DictWriter(file, diffDict.keys())
         write.writerows(diffDict)
         file.close()
@@ -239,4 +233,4 @@ class PlotCreator():
 
 if __name__ == "__main__":
     p = PlotCreator()
-    print(p.kpisw_size)
+    p.createAllDoubleImages()
